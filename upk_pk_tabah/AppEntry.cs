49c23f1189109bebfbd1343816ee2aff4127ae7f
LMFA-O <lmaokang4@gmail.com>
2025-05-12 12:56:49 +0700

    solved

---
+++ upk_pk_tabah/AppEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace upk_pk_tabah
+{
+    static class AppEntry // Atau 'Program' jika Anda memilih nama file Program.cs
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // GANTI 'Login' DENGAN NAMA FORM UTAMA ANDA JIKA BERBEDA
+            // Berdasarkan Solution Explorer Anda, sepertinya 'Login.cs' adalah form awal.
+            Application.Run(new Login());
+        }
+    }
+}
